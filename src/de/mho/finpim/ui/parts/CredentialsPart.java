44b77d022283bf7ee57b778f0e39ad2f06dc12a1
mholste <michael.holste@web.de>
2017-08-23 16:17:16 +0200

    neues feld pwd

--- src/de/mho/finpim/ui/parts/CredentialsPart.java
+++ src/de/mho/finpim/ui/parts/CredentialsPart.java
@@ -3,7 +3,7 @@ package de.mho.finpim.ui.parts;
 import org.eclipse.swt.widgets.Composite;
 
 import javax.annotation.PostConstruct;
-
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
@@ -12,6 +12,7 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 
 
@@ -22,13 +23,29 @@ public class CredentialsPart
 	@PostConstruct
 	public void createControls(Composite parent)
 	{
-		GridLayout layout = new GridLayout(2, false);
+		GridLayout layout = new GridLayout(4, false);
+		layout.marginLeft = 15;
+		layout.marginRight = 15;
+		
+		GridData grdLabel = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		GridData grdWidget = new GridData(SWT.LEFT, SWT.CENTER, true, false, 3, 1);
+		grdWidget.minimumWidth = 100;
+		
 		parent.setLayout(layout);
 		
-		Label lblAccNo = new Label(parent, SWT.NONE);
-		lblAccNo.setText("Nummer");
+		Label lblName = new Label(parent, SWT.NONE);
+		lblName.setText("Name");		
+		lblName.setLayoutData(grdLabel);
 		
-		Text txtAccNo = new Text(parent, SWT.BORDER);
+		Text txtName = new Text(parent, SWT.BORDER);
+		txtName.setLayoutData(grdWidget);
+		
+		Label lblPwd = new Label(parent, SWT.NONE);
+		lblPwd.setText("Passwort");		
+		lblPwd.setLayoutData(grdLabel);
+		
+		Text txtPwd = new Text(parent, SWT.BORDER);
+		txtPwd.setLayoutData(grdWidget);
 		
 		Button btnOK= new Button(parent, SWT.PUSH);
 		btnOK.setText("OK");
@@ -36,22 +53,9 @@ public class CredentialsPart
 		btnOK.addSelectionListener(new SelectionAdapter() {
 			@Override
 		    public void widgetSelected(SelectionEvent e) {
-		        System.out.println(txtAccNo.getText());
+		        System.out.println(txtName.getText());
 		    }
-		});
-		/*
-		txtAccNo.addModifyListener(new ModifyListener() {
-			
-			@Override
-			public void modifyText(ModifyEvent e) 
-			{
-				Text t= (Text) e.getSource(); 
-				System.out.println(t.getText());
-				
-			}
-		});
-		*/
-		
+		});		
 		
 	}
 }
