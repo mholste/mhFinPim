d9f2101c55a9f54599cf6bc2d89d3809a96c1f7c
mholste <michael.holste@web.de>
2017-08-24 18:58:52 +0200

    handker & command

--- src/de/mho/finpim/ui/parts/CredentialsPart.java
+++ src/de/mho/finpim/ui/parts/CredentialsPart.java
@@ -3,6 +3,15 @@ package de.mho.finpim.ui.parts;
 import org.eclipse.swt.widgets.Composite;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.e4.ui.di.Persist;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.MDirtyable;
+import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.e4.core.commands.ECommandService;
+import org.eclipse.e4.core.commands.EHandlerService;
+import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
@@ -11,6 +20,9 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+
+import de.mho.finpim.service.IFinPimService;
+
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -19,10 +31,21 @@ import org.eclipse.swt.layout.GridLayout;
 
 public class CredentialsPart 
 {
+	private String str;
+	
+	@Inject
+	ECommandService commandService;
+	
+	@Inject
+	EHandlerService handlerService;
+	
+	@Inject
+	IEclipseContext ctx;
 	
 	@PostConstruct
-	public void createControls(Composite parent)
+	public void createControls(Composite parent, MApplication application)
 	{
+		IEclipseContext context = application.getContext();
 		GridLayout layout = new GridLayout(4, false);
 		layout.marginLeft = 15;
 		layout.marginRight = 15;
@@ -54,8 +77,19 @@ public class CredentialsPart
 			@Override
 		    public void widgetSelected(SelectionEvent e) {
 		        System.out.println(txtName.getText());
+		        str = txtName.getText();		       
+		        ctx.set("test", str);
+		        ParameterizedCommand command = commandService.createCommand("de.mho.finpim.cmd.saveCredential", null);
+				handlerService.executeHandler(command);
 		    }
 		});		
 		
+	}
+	
+	@Persist
+	public void save(MDirtyable dirty, IFinPimService service) {
+		System.out.println("save");
+		service.savePerson(str);
+		dirty.setDirty(false);
 	}
 }
