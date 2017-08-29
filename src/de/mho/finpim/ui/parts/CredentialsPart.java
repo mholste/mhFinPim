a16999d58c15aa5484c55a716be8ad315ef96bfb
Micha <Micha@DESKTOP-61SU648.fritz.box>
2017-08-29 18:47:47 +0200

    layout angepasst

--- src/de/mho/finpim/ui/parts/CredentialsPart.java
+++ src/de/mho/finpim/ui/parts/CredentialsPart.java
@@ -3,19 +3,7 @@ package de.mho.finpim.ui.parts;
 import org.eclipse.swt.widgets.Composite;
 
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-
-import org.eclipse.e4.ui.di.Persist;
-import org.eclipse.e4.ui.model.application.MApplication;
-import org.eclipse.e4.ui.model.application.ui.MDirtyable;
-import org.eclipse.core.commands.ParameterizedCommand;
-import org.eclipse.e4.core.commands.ECommandService;
-import org.eclipse.e4.core.commands.EHandlerService;
-import org.eclipse.e4.core.contexts.IEclipseContext;
-import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Label;
@@ -26,68 +14,77 @@ import de.mho.finpim.service.IFinPimService;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 
 
 
 public class CredentialsPart 
 {
-	private String str;
-	
-	/*
-	@Inject
-	ECommandService commandService;
-	
-	@Inject
-	EHandlerService handlerService;
-	*/
+	private String user;	
+	private String pwd;
+
 	@PostConstruct
 	public void createControls(Composite parent,  IFinPimService service)
-	{
-		
+	{		
 		GridLayout layout = new GridLayout(4, false);
 		layout.marginLeft = 15;
 		layout.marginRight = 15;
-		
-		GridData grdLabel = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
 		GridData grdWidget = new GridData(SWT.LEFT, SWT.CENTER, true, false, 3, 1);
 		grdWidget.minimumWidth = 100;
 		
 		parent.setLayout(layout);
 		
 		Label lblName = new Label(parent, SWT.NONE);
-		lblName.setText("Name");		
-		lblName.setLayoutData(grdLabel);
+		lblName.setText("Name");
 		
 		Text txtName = new Text(parent, SWT.BORDER);
 		txtName.setLayoutData(grdWidget);
 		
 		Label lblPwd = new Label(parent, SWT.NONE);
 		lblPwd.setText("Passwort");		
-		lblPwd.setLayoutData(grdLabel);
 		
 		Text txtPwd = new Text(parent, SWT.BORDER);
-		txtPwd.setLayoutData(grdWidget);
+		
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
 		
 		Button btnOK= new Button(parent, SWT.PUSH);
 		btnOK.setText("OK");
 		
+		txtPwd.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent arg0) {
+				btnOK.setFocus();
+			}
+		});
+		
+		txtName.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent arg0) {
+				btnOK.setFocus();
+			}
+		});
+		
 		btnOK.addSelectionListener(new SelectionAdapter() {
 			@Override
 		    public void widgetSelected(SelectionEvent e) {
-		        System.out.println(txtName.getText());
-		        str = txtName.getText();   
-		        service.savePerson(str);
-		        //ParameterizedCommand command = commandService.createCommand("de.mho.finpim.cmd.saveCredential", null);
-				//handlerService.executeHandler(command);
+		        user = txtName.getText();   
+		        pwd = txtPwd.getText();
+		        service.saveCredentials(user, pwd);
 		    }
-		});		
+		});
 		
-	}
-	
-	@Persist
-	public void save(MDirtyable dirty, IFinPimService service) {
-		System.out.println("save");
-		service.savePerson(str);
-		dirty.setDirty(false);
 	}
 }
