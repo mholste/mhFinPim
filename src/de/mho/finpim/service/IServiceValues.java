3c1f4bf85de179c936d5712f77e550218c25da0f
Micha <Micha@DESKTOP-61SU648.fritz.box>
2017-09-03 12:29:39 +0200

    initial

---
+++ src/de/mho/finpim/service/IServiceValues.java
@@ -0,0 +1,16 @@
+package de.mho.finpim.service;
+
+public interface IServiceValues 
+{
+	public static String NAME = "Name";
+	public static String FIRSTNAME = "FName";
+	public static String USERNAME = "Username";
+	public static String PWD = "Pwd";
+	
+	public static String LOCATION = "Location";
+	public static String BLZ = "blz";
+	public static String BANK = "Bankname";
+	public static String BIC = "bic";
+	public static String ACCESS = "access";
+	public static String PIN = "PIN";
+}
