96051030684458e4ad3da62a08d9238ec651f350
mholste <michael.holste@web.de>
2017-09-05 16:21:27 +0200

    Werte entfernt

--- src/de/mho/finpim/util/GlobalValues.java
+++ src/de/mho/finpim/util/GlobalValues.java
@@ -1,16 +1,7 @@
 package de.mho.finpim.util;
 
 public interface GlobalValues 
-{
-	public static String BANK = "bank";
-	public static String LOCATION = "sitz";
-	public static String BLZ = "blz";
-	public static String BIC = "bic";
-	public static String URL = "url";
-	
-	public static String ACCESS = "access";
-	public static String PIN = "pin";
-	
+{	
 	public static String SUGGESTION = "suggest";
 	public static String BANK_LIST = "bank";
 		
