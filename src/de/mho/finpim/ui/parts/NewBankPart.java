0ce48679c8af63b1869af78e9c40333631560e21
Micha <Micha@DESKTOP-61SU648.fritz.box>
2017-09-05 22:02:00 +0200

    user zu den bankvalues

--- src/de/mho/finpim/ui/parts/NewBankPart.java
+++ src/de/mho/finpim/ui/parts/NewBankPart.java
@@ -204,6 +204,7 @@ public class NewBankPart
 				bankValues.put(IServiceValues.URL, url);
 				bankValues.put(IServiceValues.ACCESS, txtNo.getText());
 				bankValues.put(IServiceValues.PIN, txtPIN.getText());
+				bankValues.put(IServiceValues.USERNAME, (String) app.getContext().get(IServiceValues.USERNAME));   
 				
 				service.persistBank(bankValues);
 				
