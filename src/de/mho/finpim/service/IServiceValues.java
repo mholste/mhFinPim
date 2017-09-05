3480615b5c7f0b266c81260cf62b19145dc4362f
mholste <michael.holste@web.de>
2017-09-05 12:08:41 +0200

    weitere dazugefügt

--- src/de/mho/finpim/service/IServiceValues.java
+++ src/de/mho/finpim/service/IServiceValues.java
@@ -13,4 +13,9 @@ public interface IServiceValues
 	public static String BIC = "bic";
 	public static String ACCESS = "access";
 	public static String PIN = "PIN";
+	
+	public static int NOUSER = -1;
+	public static int PWD_NOK = 0;
+	public static int CREDENTIAL_OK = 1;
+	public static int USER_MULTIPLE = 2;
 }
