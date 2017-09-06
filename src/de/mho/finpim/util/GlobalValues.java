5ff6b73b4f673647347f5ba7c9d046d517af2a4d
mholste <michael.holste@web.de>
2017-09-06 08:16:34 +0200

    bank dazu

--- src/de/mho/finpim/util/GlobalValues.java
+++ src/de/mho/finpim/util/GlobalValues.java
@@ -5,5 +5,6 @@ public interface GlobalValues
 	public static String SUGGESTION = "suggest";
 	public static String BANK_LIST = "bank";
 	public static String USER ="user";
+	public static String BANK = "bank";
 		
 }
