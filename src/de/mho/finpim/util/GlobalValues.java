8dadd91a0492e44dcb6b057a2b75cb081913af8a
mholste <michael.holste@web.de>
2017-09-19 15:08:52 +0200

    String-Werte aus Interface genutzt

--- src/de/mho/finpim/util/GlobalValues.java
+++ src/de/mho/finpim/util/GlobalValues.java
@@ -6,5 +6,15 @@ public interface GlobalValues
 	public static String BANK_LIST = "bank";
 	public static String USER ="user";
 	public static String BANK = "bank";
-		
+	
+	// Werte für die HashMap zur Ablage der Konten
+	public static String ACC_BIC = "bic";
+	public static String ACC_BLZ = "blz";
+	public static String ACC_COUNTRY = "country";
+	public static String ACC_CURRENCY = "curreny";
+	public static String ACC_CUSTOMER_ID = "customerId";
+	public static String ACC_IBAN = "iban";
+	public static String ACC__NAME = "name";
+	public static String ACC_NO = "no";
+	public static String ACC_TYPE = "typ";
 }
