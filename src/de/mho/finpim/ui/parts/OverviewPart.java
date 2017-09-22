e07cb49153bcb857ccf8fc6ce49a28fc0016ab58
mho <mho@mho-Z87-HD3>
2017-09-22 10:46:56 +0200

    initial

---
+++ src/de/mho/finpim/ui/parts/OverviewPart.java
@@ -0,0 +1,14 @@
+
+package de.mho.finpim.ui.parts;
+
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class OverviewPart {
+
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		
+	}
+
+}
