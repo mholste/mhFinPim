3a23f13d6b6d776d744febc3136f61005d77dcb0
mho <mho@mho-Z87-HD3>
2017-10-02 15:04:25 +0200

    Werte für HashMap bei Pruefung der Credentials

--- src/de/mho/finpim/util/GlobalValues.java
+++ src/de/mho/finpim/util/GlobalValues.java
@@ -2,21 +2,25 @@ package de.mho.finpim.util;
 
 public interface GlobalValues 
 {	
-	public static String SUGGESTION = "suggest";
-	public static String BANK_LIST = "bank";
-	public static String USER ="user";
-	public static String BANK = "bank";
-	public static String LOCATION = "location";
+	public static final String SUGGESTION = "suggest";
+	public static final String BANK_LIST = "bank";
+	public static final String USER ="user";
+	public static final String BANK = "bank";
+	public static final String LOCATION = "location";
+	
+	// Werte für HashMap bei Pruefung der Credentials
+	public static final String STATUS = "status";
+	public static final String PERSON = "person";
 	
 	// Werte f�r die HashMap zur Ablage der Konten
-	public static String ACC_BIC = "bic";
-	public static String ACC_BLZ = "blz";
-	public static String ACC_COUNTRY = "country";
-	public static String ACC_CURRENCY = "curreny";
-	public static String ACC_CUSTOMER_ID = "customerId";
-	public static String ACC_IBAN = "iban";
-	public static String ACC__NAME = "name";
-	public static String ACC_NO = "no";
-	public static String ACC_TYPE = "typ";
-	public static String ACCOUNTS = "konten";
+	public static final String ACC_BIC = "bic";
+	public static final String ACC_BLZ = "blz";
+	public static final String ACC_COUNTRY = "country";
+	public static final String ACC_CURRENCY = "curreny";
+	public static final String ACC_CUSTOMER_ID = "customerId";
+	public static final String ACC_IBAN = "iban";
+	public static final String ACC__NAME = "name";
+	public static final String ACC_NO = "no";
+	public static final String ACC_TYPE = "typ";
+	public static final String ACCOUNTS = "konten";
 }
