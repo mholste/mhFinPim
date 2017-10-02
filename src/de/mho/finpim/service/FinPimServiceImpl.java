d75b0cf84bda066129cab6204e5df50714809303
mho <mho@mho-Z87-HD3>
2017-10-02 15:15:49 +0200

    checkCreddentials mitRückgabepartameter als HashMap implementiert

--- src/de/mho/finpim/service/FinPimServiceImpl.java
+++ src/de/mho/finpim/service/FinPimServiceImpl.java
@@ -36,20 +36,26 @@ public class FinPimServiceImpl implements IFinPimService
 {
 	
 	/**
-	 * Implementierung der Interface-Methode. Checkt, ob die Kombination aus User/Passwort in der Datenbak 
-	 * vorhanden und korrekt ist. 
+	 * Implementierung der Interface-Methode. Checkt, ob die Kombination aus User/Passwort 
+	 * in der Datenbak vorhanden und korrekt ist.  Gibt im Key <code>GlobalValues.STATUS</code>
+	 * das Ergebnis der Prüfung zurück. Im Key <code>GolbalValues.PERSON</code> steht im 
+	 * Erfolgsfall das entsprechende <code>Person</code>-Objekt.
 	 * @param user Der eingegebene Username
 	 * @param pwd Daseingegebne Passwort
-	 * @return int -1 Der User ist in der DB nicht vorhanden
+	 * 
+	 * @return HashMap
+	 * 				Mögliche Werte im Key STATUS
+	 * 				-1 Der User ist in der DB nicht vorhanden
 	 *              0 Das Passwort fï¿½r den User ist nicht korrekt
 	 *              1 Die Kombination ist korrekt
 	 *              2 Der User ist mehrfach vorhanden 
 	 */
 	@SuppressWarnings("unchecked")
 	@Override
-	public int checkCedentials(String user, String pwd) 
+	public HashMap checkCedentials(String user, String pwd) 
 	{	
-		//return IServiceValues.CREDENTIAL_OK;
+		int checkCredential;
+		HashMap<String,Object> retVal= new HashMap<String, Object>();
 		
 		EntityManager em = Activator.getEntityManager();
 		
@@ -59,19 +65,26 @@ public class FinPimServiceImpl implements IFinPimService
 		
 		if (persons.isEmpty())
 		{
-			return IServiceValues.NOUSER;
+			checkCredential =  IServiceValues.NOUSER;
 		} 
 		else if (persons.size() > 1)
 		{ 
-			return IServiceValues.USER_MULTIPLE;
+			checkCredential =  IServiceValues.USER_MULTIPLE;
 		}
 		else if (persons.get(0).getPwd().equals(pwd))
 		{
-			return IServiceValues.CREDENTIAL_OK;
+			checkCredential = IServiceValues.CREDENTIAL_OK;
 		}
-		return IServiceValues.PWD_NOK;
-		
-		
+		else 
+		{
+			checkCredential = IServiceValues.PWD_NOK;
+		}
+		retVal.put(GlobalValues.STATUS, new Integer(checkCredential));
+		if (persons.size() > 0)
+		{
+			retVal.put(GlobalValues.PERSON, persons.get(0));
+		}
+		return retVal;		
 	}
 
 	@Override
@@ -97,6 +110,11 @@ public class FinPimServiceImpl implements IFinPimService
 	public Bank persistBank(HashMap values)
 	{
 		EntityManager em = Activator.getEntityManager();
+		
+		Person p = (Person) em.createQuery("SELECT p FROM Person p WHERE p.uName=:arg")
+				.setParameter("arg", values.get(IServiceValues.USERNAME)).getSingleResult();
+		//Person p = persons.get(0);
+		
 		em.getTransaction().begin();
 		
 		Bank b = new Bank();
@@ -106,13 +124,10 @@ public class FinPimServiceImpl implements IFinPimService
 		b.setBic((String)values.get(IServiceValues.BIC));
 		b.setAccessCode((String)values.get(IServiceValues.ACCESS));
 		b.setPIN((String)values.get(IServiceValues.PIN));
+		b.setPerson(p);
 		
 		em.persist(b);
-		em.getTransaction().commit();
-		
-		List<Person> persons = (List<Person>) em.createQuery("SELECT p FROM Person p WHERE p.uName=:arg")
-				.setParameter("arg", values.get(IServiceValues.USERNAME)).getResultList();
-		Person p = persons.get(0);
+		em.getTransaction().commit();				
 		
 		em.getTransaction().begin();
 		p.addBank(b);
