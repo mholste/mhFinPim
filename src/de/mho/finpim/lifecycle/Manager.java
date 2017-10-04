d20952b52e698e273e6025563dc97fd7a8a5537e
mho <mho@mho-Z87-HD3>
2017-10-04 09:33:22 +0200

    Arbeitsverzeichnis gesetzt und als Wert für client.passport.PinTan.filename in properties geschrieben

--- src/de/mho/finpim/lifecycle/Manager.java
+++ src/de/mho/finpim/lifecycle/Manager.java
@@ -1,11 +1,15 @@
 package de.mho.finpim.lifecycle;
 
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.e4.ui.workbench.lifecycle.PostContextCreate;
 import org.eclipse.equinox.app.IApplicationContext;
 
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Manager 
 {
@@ -13,23 +17,37 @@ public class Manager
 	
 	
     @PostContextCreate
-    public void postContextCreate(IApplicationContext appContext) {
-        
-        // close the static splash screen
+    public void postContextCreate(IApplicationContext appContext) throws IllegalStateException, IOException 
+    {
+    	String path = "";
         appContext.applicationRunning();
 
-        System.out.println("Lifecycle");
-        /*
-        initializer = DBInit.getInstance();
-        
-        try
+        if (!Platform.getInstanceLocation().isSet()) 
         {
-        Connection conn = initializer.getConnection();
+            String defaultPath = System.getProperty("user.home");
+            path = defaultPath + "/mhFinpim/";
+            URL instanceLocationUrl = new URL("file", null, path);
+            Platform.getInstanceLocation().set(instanceLocationUrl, false);
         }
-        catch (SQLException se) {
-			// TODO: Logging rein...
-        	System.out.println("Verbindung zur DB fehlgeschlagen");
-		}
-        */
+        setWorkLocation(path);
+        
+    }
+    
+    private void setWorkLocation(String path)
+    {
+    	String ppFile = path + "/pp.dat";
+    	Properties prop = new Properties();
+        URL url;
+        
+        try 
+        {
+            url = new URL("platform:/plugin/mhFinPim/files/hbci.properties");
+            InputStream inputStream = url.openConnection().getInputStream();
+            prop.load(inputStream);
+            prop.setProperty("client.passport.PinTan.filename", path);
+         
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
