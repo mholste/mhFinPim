bcb02469063d9105084ac39bf280cf93cec585e1
mholste <michael.holste@web.de>
2018-01-15 20:19:53 +0100

    gelöscht

--- src/de/mho/finpim/service/IFinPimBanking.java
+++ src/de/mho/finpim/service/IFinPimBanking.java
@@ -2,11 +2,16 @@ package de.mho.finpim.service;
 
 import java.util.ArrayList;
 
+import de.mho.finpim.persistence.model.Account;
 import de.mho.finpim.persistence.model.Bank;
 
 public interface IFinPimBanking 
 {
 
 	public ArrayList fetchAccounts(Bank b);
+	
+	public Object getAccountBalace(Account acc);
+	
+	public Object getAccountBalance(String accNo, Bank b);
 
 }
