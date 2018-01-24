679908debdee9111ed1f6779bfb39a086ab13f93
mho <michael.holste@web.de>
2018-01-24 11:50:59 +0100

    Servicemthoden für die Verwaltung der Vorschläge zu den Banken im UI

--- src/de/mho/finpim/service/IPlatformDataService.java
+++ src/de/mho/finpim/service/IPlatformDataService.java
@@ -1,6 +1,18 @@
 package de.mho.finpim.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public interface IPlatformDataService 
 {
 	public void hello();
+	
+	// Methoden für die VerwaLtung der Bankvorschläge im UI
+	
+	public void setBankingListValues(ArrayList<String> suggest, HashMap<String, Map> banks);
+	
+	public ArrayList<String> getSuggestions();
+	
+	public HashMap<String, Map> getBankList();
 }
--- src/de/mho/finpim/service/PlatformDataServiceImpl.java
+++ src/de/mho/finpim/service/PlatformDataServiceImpl.java
@@ -1,11 +1,36 @@
 package de.mho.finpim.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class PlatformDataServiceImpl implements IPlatformDataService 
 {
-
+	private ArrayList suggestion;
+	private HashMap bankList;
+	
 	@Override
 	public void hello() 
 	{
 		System.out.println("test it");
+	}
+
+	@Override
+	public void setBankingListValues(ArrayList<String> suggest, HashMap<String, Map> banks) 
+	{
+		this.suggestion = suggest;
+		this.bankList = banks;
+	}
+
+	@Override
+	public ArrayList<String> getSuggestions() 
+	{
+		return this.suggestion;
+	}
+
+	@Override
+	public HashMap<String, Map> getBankList() 
+	{
+		return this.bankList;
 	}
 }
