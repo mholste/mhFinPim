e4aaad470ea9f3bbb2946b82ec09cb9317a60b71
mho <michael.holste@web.de>
2018-02-27 14:59:42 +0100

    Name der Methode für Kontoauszüge umbenannt

--- src/de/mho/finpim/service/IFinPimBanking.java
+++ src/de/mho/finpim/service/IFinPimBanking.java
@@ -14,5 +14,5 @@ public interface IFinPimBanking
 	
 	public String getAccountBalace(Account acc);
 	
-	public ArrayList<HashMap<String, Object>> getBalanceValues(Account acc);
+	public ArrayList<HashMap<String, Object>> getStatementList(Account acc);
 }
